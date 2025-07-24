c61a7f643f290d88f32c8834be588663712feb97
Casey Hunt <33342229+protothesis@users.noreply.github.com>
2025-07-23 22:28:59 -0500

    more deploy bullshit

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://protothesis.github.io',
-	// base: '/',
+	// base: '../docs',
 	integrations: [mdx(), sitemap()],
 });
